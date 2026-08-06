3c16c8fabdc6bed99f3cd3ed6b2150adcb1f5d86
nissanthapaz99-cmd <nissanthapaz99@gmail.com>
2026-08-06 20:27:31 +1000

    Initial MVC Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SocialExposure.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
