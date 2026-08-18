ac6a199a053efd0e9e7b61ba96343313a7f103ae
namuna-shakya <shakyanamuna44@gmail.com>
2026-08-18 22:14:40 +1000

    making some buttons clickable

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,4 +21,18 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    public IActionResult About()
+{
+    return View();
+}
+
+public IActionResult Contact()
+{
+    return View();
+}
+
+public IActionResult Help()
+{
+    return View();
+}
 }
