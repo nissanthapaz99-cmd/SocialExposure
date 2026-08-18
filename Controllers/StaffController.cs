a73deafb0897ac3311fe9cba8fabd60ad442f0f2
namuna-shakya <shakyanamuna44@gmail.com>
2026-08-18 15:22:09 +1000

    minor changes

--- Controllers/StaffController.cs
+++ Controllers/StaffController.cs
@@ -1,16 +1,29 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SocialExposure.Models;
+using SocialExposure.Data;
 
 namespace SocialExposure.Controllers
 {
     [Authorize(Roles = UserRoles.Admin + "," + UserRoles.Staff)]
     public class StaffController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public StaffController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Dashboard()
         {
-            return View();
+            var events = _context.Events
+                .Where(e => e.Status != "Completed")
+                .OrderByDescending(e => e.Id)
+                .ToList();
+
+            return View(events);
         }
     }
 }
